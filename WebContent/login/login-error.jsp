f21ba512621fbd42382cbec58bf448855991e62d
djangouser <user@wxample.com>
2024-07-03 11:07:18 +0900

    関：ログイン

---
+++ WebContent/login/login-error.jsp
@@ -0,0 +1,26 @@
+<%-- 学生一覧JSP --%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<link rel="stylesheet" href="/css/bootstrap.min.css" />
+
+<%-- 文字化けの対策 --%>
+<% request.setCharacterEncoding("UTF-8"); %>
+
+<c:import url="/common/base.jsp">
+    <c:param name="title">
+        <h1 class="toptitle">得点管理システム</h1>
+
+    </c:param>
+
+    <c:param name="scripts"></c:param>
+
+    <c:param name="content">
+        <section class="mo-4">
+
+            <div class="my-2 text-end px-4">
+                <a>システムエラーだぜ！！</a>
+            </div>
+        </section>
+    </c:param>
+</c:import>
---
+++ src/Login/LoginAction.java
@@ -0,0 +1,47 @@
+package Login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import dao.TeacherDAO;
+import tool.Page;
+
+@WebServlet(urlPatterns={"/login/loginaction"})
+public class LoginAction extends HttpServlet {
+
+	public void doGet (
+		HttpServletRequest request, HttpServletResponse response
+	) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		Page.header(out);
+		try {
+		HttpSession session=request.getSession();
+
+		String login=request.getParameter("userId");
+		String password=request.getParameter("password");
+		TeacherDAO dao = new TeacherDAO();
+		Teacher teacher = dao.login(login, password);
+
+		if (teacher!=null){
+			session.setAttribute("teacher", teacher);
+			request.getRequestDispatcher("/mein/menu.jsp")
+			.forward(request, response);
+		}
+		request.setAttribute("errorMessage","ユーザ名またはパスワードが違います");
+		request.getRequestDispatcher("/login/login.jsp")
+		.forward(request, response);
+
+		} catch (Exception e) {
+			e.printStackTrace(out);
+		}
+		Page.footer(out);
+	}
+}
