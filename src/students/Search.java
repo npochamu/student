53d006203ff103bd3041d6981b249978d580ca2a
djangouser <user@wxample.com>
2024-07-26 09:37:15 +0900

    絞り込み修正:関

--- src/students/Search.java
+++ src/students/Search.java
@@ -58,7 +58,7 @@ public class Search extends HttpServlet {
 
 			StudentsDAO dao=new StudentsDAO();
 
-			if(!(class_no.equals("")) && (ent_year.equals("") || is_attend.equals("false"))){
+			if(!("".equals(class_no)) && ("".equals(ent_year) || "false".equals(is_attend))){
 				ent_year=request.getParameter("year2");
 				class_no=request.getParameter("classsno2");
 				is_attend=request.getParameter("zaigaku2");
@@ -74,20 +74,20 @@ public class Search extends HttpServlet {
                 request.setAttribute("errorMessage", "クラスを指定する場合は入学年度も指定してください");
 
 			}
-			if((!(ent_year.equals("")) || is_attend == "false") && class_no.equals("")){
+			if((!("".equals(ent_year))) && "".equals(class_no)){
 				System.out.println("サメ1");
 
 				List<Student> list=dao.search(ent_year, isAttend);
 				System.out.print(list);
 				request.setAttribute("students", list);
 
-			}else if(!(ent_year.equals("")) || !(class_no.equals("")) || is_attend.equals("true")){
+			}else if((!("".equals(ent_year)) && (!("".equals(class_no))))){
 				System.out.println("サメ");
 
 				List<Student> list=dao.search(ent_year, class_no, isAttend);
 				request.setAttribute("students", list);
 
-			}else if(is_attend.equals("true") && (ent_year.equals("") && class_no.equals(""))){
+			}else if("true".equals(is_attend) && "".equals(ent_year) && "".equals(class_no)){
 				List<Student> list=dao.search(isAttend);
 				request.setAttribute("students", list);
 			}else{
